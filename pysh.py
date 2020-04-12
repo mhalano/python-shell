0ae9550dd6b4652fef9588079042a3a7dc89dc17
Marcos Alano <marcoshalano@gmail.com>
2020-04-12 19:04:06 -0300

    Add handle to KeyboardInterrupt exception

--- pysh.py
+++ pysh.py
@@ -1,3 +1,17 @@
+from os import system
+
+
 while True:
-    command = input("$ ")
+
+    try:
+        command = input("$ ")
+    except KeyboardInterrupt:
+        exit(1)
+
+    if command == "exit" or command == "quit":
+        print("Hasta la vista, baby!")
+        exit(0)
+    else:
+        system(command)
+
 
